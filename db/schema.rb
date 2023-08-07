70970f6e90397ef67d8116c1041b2a23455e1291
Arpan <arpan.patel00@yahoo.com>
2023-08-07 19:03:18 +0530

    Add tests and modify rspec.yml for DB test prepeare

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_06_123740) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_07_125730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
